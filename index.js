f03a92d656a21b2bb96f416b96d222122aa16e06
Đinh Vạn Tài <vantaidinh04@gmail.com>
2025-03-26 15:33:12 +0700

    Delete the function of adding a contact

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ app.get('/contacts', (req, res) => {
     res.json(contacts);
 });
 
-app.post('/contacts', (req, res) => {
-    const { name, phone, email } = req.body;
-    if (!name || !phone) {
-      return res.status(400).json({ error: 'Name and phone are required' });
-    }
-    const newContact = { id: contacts.length + 1, name, phone, email };
-    contacts.push(newContact);
-    res.status(201).json(newContact);
-  });
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
