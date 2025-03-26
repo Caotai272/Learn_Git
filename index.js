05c3c03b922cc073fc24fdc2d7fc7a4d75c6827b
TaiCA <caoanhtai2702@gmail.com>
2025-03-26 15:05:39 +0700

    Create projeck Node.js with API

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let contacts = [];
+
+app.get('/contacts', (req, res) => {
+  res.json(contacts);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
